82281e880c3732f8fd8fef1ba902ad04f04e328c
Gala Chernikova <gala.chernikova94@gmail.com>
2022-12-21 15:12:32 +0300

    Fix slider

--- fargo.js
+++ fargo.js
@@ -1,48 +1,47 @@
 //menu
-const navBtn = document.querySelector('.nav-button');
-const mobileNav = document.querySelector('.mobile-nav');
+const navBtn = document.querySelector(".nav-button");
+const mobileNav = document.querySelector(".mobile-nav");
 const body = document.body;
 
 function toggleMobileNav() {
-    mobileNav.classList.toggle('mobile-nav-active');
-    navBtn.classList.toggle('nav-button-close');
-    body.classList.toggle('no-scroll');
+  mobileNav.classList.toggle("mobile-nav-active");
+  navBtn.classList.toggle("nav-button-close");
+  body.classList.toggle("no-scroll");
 }
 
-navBtn.addEventListener('click', function(event){
-    event.stopPropagation();
+navBtn.addEventListener("click", function (event) {
+  event.stopPropagation();
+  toggleMobileNav();
+});
+
+window.addEventListener("click", function () {
+  if (body.classList.contains("no-scroll")) {
     toggleMobileNav();
-})
+  }
+});
 
-window.addEventListener('click', function(){
-    if (body.classList.contains('no-scroll')) {
-        toggleMobileNav();
-    }
-})
-
-mobileNav.addEventListener('click', function(event){
-    event.stopPropagation();
-})
-
+mobileNav.addEventListener("click", function (event) {
+  event.stopPropagation();
+});
 
 //slider
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 10,
-    slidesPerView: 5,
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
-  var swiper2 = new Swiper(".mySwiper2", {
-    spaceBetween: 10,
-    pagination: {
-        el: ".swiper-pagination",
-        type: "fraction",
-      },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-      swiper: swiper,
-    },
-  });
+  spaceBetween: 10,
+  slidesPerView: 5,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
